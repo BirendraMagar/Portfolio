096cf2120936914cfa72b8cf3d55a886d025cd27
Sebastian107 <abirendra51@gmail.com>
2023-09-26 12:25:26 +0545

    Slight changes

--- lib/data.ts
+++ lib/data.ts
@@ -39,7 +39,7 @@ export const experiencesData = [
     description:
       "I completed my 3 months of intern. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2023 March",
   },
   {
     title: "Front-End Developer",
@@ -52,24 +52,24 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Nep Tech Pal",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "Official website of the IT company Nep Tech Pal. I worked as a frontend developer on this project.",
+    tags: ["React", "Next.js", "Tailwind", "Framer Motion", "Figma"],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: "Footstep Education ",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      "Educaiton Consultancy in Pokhara. I was the front-end developer. It has features like data from Contentful.",
+    tags: ["React", "Next.js", "Tailwind", "Contentful", "Formspree"],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: "Peace Ecommerce",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      "A test project with basic features of an E-commerce website like categories, add to cart, checkout, etc.",
+    tags: ["React", "Redux", "Material UI", "Strapi", "Stripe"],
     imageUrl: wordanalyticsImg,
   },
 ] as const;
